6640f07b24c5203502c273fd28b240649e0b8e24
Rainbo <jesselyn.jackson@gmail.com>
2019-06-20 21:31:00 -0400

    Moved random number generator inside of click function and displayed the number in the DOM targeting an ID of firstNumber.

--- assets/javascript/lottery.js
+++ assets/javascript/lottery.js
@@ -1,14 +1,19 @@
-//wait until page loads
-$(document).ready(function(){
+var randomNum;
 
-    //create a nine digit random number
-    var randomNum = Math.floor(10000000 + Math.random() * 900000000);
-    console.log("randomNum: ", randomNum);
+//wait until page loads
+$(document).ready(function () {
+
+
 
 
 
     //create button listener
-    $("button").click(function(){
+    $("button").click(function () {
+        //create a nine digit random number
+        randomNum = Math.floor(10000000 + Math.random() * 900000000);
+        console.log("randomNum: ", randomNum);
+        //I need an element to modify in the DOM
+        var presentNum = $("#firstNumber").text(randomNum);
 
     });
 });
