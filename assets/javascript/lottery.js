cd3be49aafc1242741fa56ba67190a6e3a3737ef
Rainbo <jesselyn.jackson@gmail.com>
2019-06-20 21:02:22 -0400

    Added the document ready function and button listener.

---
+++ assets/javascript/lottery.js
@@ -0,0 +1,14 @@
+//wait until page loads
+$(document).ready(function(){
+
+    //create a nine digit random number
+    var randomNum = Math.floor(10000000 + Math.random() * 900000000);
+    console.log("randomNum: ", randomNum);
+
+
+
+    //create button listener
+    $("button").click(function(){
+
+    });
+});
