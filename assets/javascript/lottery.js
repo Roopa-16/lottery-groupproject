9585c80e035115d49bb9c3e486f530ea4801f460
Rainbo <jesselyn.jackson@gmail.com>
2019-06-22 14:12:57 -0400

    Appeded the lottery number in new <p> to the #firstNumber div

--- assets/javascript/lottery.js
+++ assets/javascript/lottery.js
@@ -10,7 +10,9 @@ $(document).ready(function () {
         randomNum = Math.floor(10000000 + Math.random() * 900000000);
         console.log("randomNum: ", randomNum);
         //I need an element to modify in the DOM
-        var presentNum = $("#firstNumber").text(randomNum);
+        $("#firstNumber").append("<p>" + randomNum + "</p>");
+
+        //Need to create additional divs and append them to firstNumber to hold more randomNums
 
     });
 });
